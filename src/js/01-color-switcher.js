a7e84d7a7b31916877f1321c9b9f3b7faaf6c1fa
KizimJule <kizimjule@gmail.com>
2022-09-05 21:21:50 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,6 +1,7 @@
 const startBtn = document.querySelector('[data-start]');
 const stopBtn = document.querySelector('[data-stop]');
 const body = document.querySelector('body');
+let timerId = null;
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
@@ -8,7 +9,7 @@ function getRandomHexColor() {
 stopBtn.setAttribute('disabled', true);
 
 startBtn.addEventListener('click', () => {
-  let timerId = setInterval(() => {
+  timerId = setInterval(() => {
     const color = getRandomHexColor();
     body.style.backgroundColor = color;
   }, 1000);
